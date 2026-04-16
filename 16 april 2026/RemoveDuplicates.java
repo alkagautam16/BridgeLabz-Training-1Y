6e7f7a9f2bac1073f1eff1e76b738d3e1f4f136d
alkagautam16 <alkagautam1601@gmail.com>
2026-04-16 15:51:06 +0530

    16 april 2026

---
+++ 16 april 2026/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        
+    
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(20);
+        list.add(10);
+        list.add(40);
+
+        System.out.println("Original List: " + list);
+
+        
+        HashSet<Integer> set = new HashSet<>(list);
+
+     
+        ArrayList<Integer> uniqueList = new ArrayList<>(set);
+
+        System.out.println("List after removing duplicates: " + uniqueList);
+    }
+}
